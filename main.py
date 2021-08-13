8a59cd4ba545b18568e9ce0d72c12d4b165b6e9d
DmytroZeleniukh93 <d.zeleniukh1@gmail.com>
2021-08-13 11:56:09 +0300

    lesson_2

--- main.py
+++ main.py
@@ -7,8 +7,17 @@ class Window:
         self.canvas.geometry('400x400+500+200')
         self.canvas.title('My window')
         self.canvas.resizable(False, False)
+        self.label = Label(self.canvas, text='Label 1', bg='#42f5c2') # color picker
+
+    def draw_widgets(self):
+        self.label.pack(anchor=NW)
+        Button(self.canvas, text='Go!', command=self.button_action).pack()
+
+    def button_action(self):
+        self.label.config(text='Goooo!')
 
     def run(self):
+        self.draw_widgets()
         self.canvas.mainloop()
 
 
